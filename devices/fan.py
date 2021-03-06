a8e8f3e41607369d6808b430b9066bad21a942fa
Aleksandr Dmitriev <sh2533@gmail.com>
2021-03-06 18:57:41 +0000

    fix fan

--- devices/fan.py
+++ devices/fan.py
@@ -4,23 +4,26 @@
 # with RPi.GPIO - requires RPi.GPIO 0.5.2a or higher  
 
 import RPi.GPIO as GPIO # always needed with RPi.GPIO  
+import time
 
 GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM  
 
-GPIO.setup(25, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port  
+GPIO.setup(17, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port  
 
-p = GPIO.PWM(25, 50)    # create an object p for PWM on port 25 at 50 Hertz  
+p = GPIO.PWM(17, 25000)    # create an object p for PWM on port 25 at 50 Hertz  
 # you can have more than one of these, but they need
 # different names for each port
 # e.g. p1, p2, motor, servo1 etc.
 
-p.start(50)             # start the PWM on 50 percent duty cycle  
+p.start(100)             # start the PWM on 50 percent duty cycle  
 # duty cycle value can be 0.0 to 100.0%, floats are OK
 
-p.ChangeDutyCycle(90)   # change the duty cycle to 90%  
+p.ChangeDutyCycle(10)   # change the duty cycle to 90%  
 
-p.ChangeFrequency(100)  # change the frequency to 100 Hz (floats also work)  
+#p.ChangeFrequency(100)  # change the frequency to 100 Hz (floats also work)  
 # e.g. 100.5, 5.2
+
+time.sleep(10)
 
 p.stop()                # stop the PWM output  
 
