95a51aad3867055f38fab8e8a47dc8f98a0a8aa5
Aleksandr Dmitriev <shurik2533@ya.ru>
2021-03-06 22:04:45 +0300

    2 fans experiment

--- GPIO.md
+++ GPIO.md
@@ -1,2 +1,4 @@
 # Reserved GPIO pins
 - 4 - thermometers
+- 17 - fan1
+- 27 - fan2
--- devices/fan.py
+++ devices/fan.py
@@ -8,23 +8,26 @@ import time
 
 GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM  
 
-GPIO.setup(17, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port  
+GPIO.setup(17, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port
+GPIO.setup(27, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port
 
-p = GPIO.PWM(17, 25000)    # create an object p for PWM on port 25 at 50 Hertz  
+p1 = GPIO.PWM(17, 25000)    # create an object p for PWM on port 25 at 50 Hertz
+p2 = GPIO.PWM(27, 25000)    # create an object p for PWM on port 25 at 50 Hertz
 # you can have more than one of these, but they need
 # different names for each port
 # e.g. p1, p2, motor, servo1 etc.
 
-p.start(100)             # start the PWM on 50 percent duty cycle  
+p1.start(50)             # start the PWM on 50 percent duty cycle
+p2.start(10)             # start the PWM on 50 percent duty cycle
 # duty cycle value can be 0.0 to 100.0%, floats are OK
 
-p.ChangeDutyCycle(10)   # change the duty cycle to 90%  
+#p1.ChangeDutyCycle(10)   # change the duty cycle to 90%
 
-#p.ChangeFrequency(100)  # change the frequency to 100 Hz (floats also work)  
 # e.g. 100.5, 5.2
 
 time.sleep(10)
 
-p.stop()                # stop the PWM output  
+p1.stop()
+p2.stop()        # stop the PWM output
 
 GPIO.cleanup()          # when your program exits, tidy up after yourself
