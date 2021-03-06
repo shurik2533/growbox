10d96c7c79df9b27b7b69b261d7db09914a508a9
Aleksandr Dmitriev <shurik2533@ya.ru>
2021-03-06 21:52:35 +0300

    change pwm control

--- devices/fan.py
+++ devices/fan.py
@@ -1,9 +1,27 @@
-from RPIO import PWM
+# Don't try to run this as a script or it will all be over very quickly  
+# it won't do any harm though.  
+# these are all the elements you need to control PWM on 'normal' GPIO ports  
+# with RPi.GPIO - requires RPi.GPIO 0.5.2a or higher  
 
-servo = PWM.Servo()
+import RPi.GPIO as GPIO # always needed with RPi.GPIO  
 
-# Set servo on GPIO17 to 1200µs (1.2ms)
-servo.set_servo(17, 1200)
+GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM  
 
-# Clear servo on GPIO17
-servo.stop_servo(17)
+GPIO.setup(25, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port  
+
+p = GPIO.PWM(25, 50)    # create an object p for PWM on port 25 at 50 Hertz  
+# you can have more than one of these, but they need
+# different names for each port
+# e.g. p1, p2, motor, servo1 etc.
+
+p.start(50)             # start the PWM on 50 percent duty cycle  
+# duty cycle value can be 0.0 to 100.0%, floats are OK
+
+p.ChangeDutyCycle(90)   # change the duty cycle to 90%  
+
+p.ChangeFrequency(100)  # change the frequency to 100 Hz (floats also work)  
+# e.g. 100.5, 5.2
+
+p.stop()                # stop the PWM output  
+
+GPIO.cleanup()          # when your program exits, tidy up after yourself
