08f3ebd5fea40096622dc2fee5cdfd1a2a8cbaae
Aleksandr Dmitriev <sh2533@gmail.com>
2021-03-07 15:12:35 +0000

    experiment with fan

--- devices/fan.py
+++ devices/fan.py
@@ -9,25 +9,86 @@ import time
 GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM  
 
 GPIO.setup(17, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port
-GPIO.setup(21, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port
+GPIO.setup(27, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port
 
-p1 = GPIO.PWM(17, 25000)    # create an object p for PWM on port 25 at 50 Hertz
-p2 = GPIO.PWM(21, 25000)    # create an object p for PWM on port 25 at 50 Hertz
+p1 = GPIO.PWM(17, 1000)    # create an object p for PWM on port 25 at 50 Hertz
+p2 = GPIO.PWM(27, 1000)    # create an object p for PWM on port 25 at 50 Hertz
 # you can have more than one of these, but they need
 # different names for each port
 # e.g. p1, p2, motor, servo1 etc.
-
-p1.start(0)             # start the PWM on 50 percent duty cycle
-p2.start(0)             # start the PWM on 50 percent duty cycle
+print(100)
+p1.start(100)             # start the PWM on 50 percent duty cycle
+p2.start(100)             # start the PWM on 50 percent duty cycle
 # duty cycle value can be 0.0 to 100.0%, floats are OK
 
 #p1.ChangeDutyCycle(10)   # change the duty cycle to 90%
 
 # e.g. 100.5, 5.2
 
-time.sleep(15)
+time.sleep(5)
+
 #
-# p1.ChangeDutyCycle(0)
+print(90)
+p1.ChangeDutyCycle(90)
+p2.ChangeDutyCycle(90)
+time.sleep(5)
+
+
+print(80)
+p1.ChangeDutyCycle(80)
+p2.ChangeDutyCycle(80)
+time.sleep(5)
+
+
+print(70)
+p1.ChangeDutyCycle(70)
+p2.ChangeDutyCycle(70)
+time.sleep(5)
+
+
+print(60)
+p1.ChangeDutyCycle(60)
+p2.ChangeDutyCycle(60)
+time.sleep(5)
+
+
+print(50)
+p1.ChangeDutyCycle(50)
+p2.ChangeDutyCycle(50)
+time.sleep(5)
+
+
+print(40)
+p1.ChangeDutyCycle(40)
+p2.ChangeDutyCycle(40)
+time.sleep(5)
+
+
+print(30)
+p1.ChangeDutyCycle(30)
+p2.ChangeDutyCycle(30)
+time.sleep(5)
+
+print(20)
+p1.ChangeDutyCycle(20)
+p2.ChangeDutyCycle(20)
+time.sleep(5) 
+
+print(10)
+p1.ChangeDutyCycle(10)
+p2.ChangeDutyCycle(10)
+time.sleep(5) 
+
+print(5)
+p1.ChangeDutyCycle(5)
+p2.ChangeDutyCycle(5)
+time.sleep(5) 
+
+print(0)
+p1.ChangeDutyCycle(0)
+p2.ChangeDutyCycle(0)
+time.sleep(5) 
+
 # p2.ChangeDutyCycle(100)
 #
 # time.sleep(5)
