4034142b4b3a3afb8deead3986aef3e1729abe4e
Aleksandr Dmitriev <shurik2533@ya.ru>
2021-03-10 23:12:44 +0300

    change logger

--- logger.py
+++ logger.py
@@ -1,22 +1,11 @@
 import logging
 import json_logging
+import sys
 
 
 def get_logger():
-    """
-    Generic utility function to get logger object with fixed configurations
-    :return:
-    logger object
-    """
-    json_logging.ENABLE_JSON_LOGGING = True
-    json_logging.init_non_web()
+    json_logging.init_non_web(enable_json=True)
     logger = logging.getLogger(__name__)
-    logging.basicConfig()
-    json_logging.config_root_logger()
     logger.setLevel(logging.DEBUG)
-    logging.addLevelName(logging.ERROR, 'error')
-    logging.addLevelName(logging.WARNING, 'warning')
-    logging.addLevelName(logging.INFO, 'info')
-    logging.addLevelName(logging.DEBUG, 'debug')
-    logger.addHandler(logging.NullHandler())
+    logger.addHandler(logging.StreamHandler(sys.stdout))
     return logger
