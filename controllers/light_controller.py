d87892a9e8b585cb076d7e7d8ae52c27b0b24fdb
Aleksandr Dmitriev <shurik2533@ya.ru>
2021-05-22 20:37:54 +0300

    WIP

--- controllers/light_controller.py
+++ controllers/light_controller.py
@@ -1,5 +1,11 @@
+from datetime import datetime
+
 from devices import relay
 from devices.relay import LIGHT_1, LIGHT_2
+
+DAY_STARTS_AT = '19:00'
+DAY_ENDS_AT = '07:00'
+LIGHT_MODE = 'both' # one/both
 
 
 class LightController:
@@ -7,10 +13,37 @@ class LightController:
         self.state = state
 
     def control(self):
-        if self.state['light']['1'] == 'OFF':
-            relay.on(LIGHT_1)
-            self.state['light']['1'] = 'ON'
+        if self._is_light_on():
+            if LIGHT_MODE == 'both':
+                relay.on(LIGHT_1)
+                self.state['light']['1'] = 'ON'
+                relay.on(LIGHT_2)
+                self.state['light']['2'] = 'ON'
+            else:
+                relay.on(LIGHT_1)
+                self.state['light']['1'] = 'ON'
+        else:
+            if LIGHT_MODE == 'both':
+                relay.off(LIGHT_1)
+                self.state['light']['1'] = 'OFF'
+                relay.off(LIGHT_2)
+                self.state['light']['2'] = 'OFF'
+            else:
+                relay.off(LIGHT_1)
+                self.state['light']['1'] = 'OFF'
 
-        if self.state['light']['2'] == 'OFF':
-            relay.on(LIGHT_2)
-            self.state['light']['2'] = 'ON'
+    @staticmethod
+    def _is_light_on():
+        now = datetime.now()
+        minutes_since_midnight = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()/60
+
+        def get_minutes(s):
+            arr = [int(v) for v in DAY_STARTS_AT.split(':')]
+            return arr[0] * 60 + arr[1]
+        minutes_start = get_minutes(DAY_STARTS_AT)
+        minutes_end = get_minutes(DAY_ENDS_AT)
+        if minutes_start > minutes_end:
+            return (0 <= minutes_since_midnight <= minutes_end) or (minutes_start <= minutes_since_midnight <= 24*60)
+        else:
+            return minutes_start <= minutes_since_midnight <= minutes_end
+
--- logger.py
+++ logger.py
@@ -1,13 +1,35 @@
+import datetime
 import logging
 import json_logging
+import pytz
 import sys
 
+
+class Formatter(logging.Formatter):
+    """override logging.Formatter to use an aware datetime object"""
+    def converter(self, timestamp):
+        dt = datetime.datetime.fromtimestamp(timestamp)
+        tzinfo = pytz.timezone('Europe/Moscow')
+        return tzinfo.localize(dt)
+
+    def formatTime(self, record, datefmt=None):
+        dt = self.converter(record.created)
+        if datefmt:
+            s = dt.strftime(datefmt)
+        else:
+            try:
+                s = dt.isoformat(timespec='milliseconds')
+            except TypeError:
+                s = dt.isoformat()
+        return s
 
 def _get_logger():
     json_logging.init_non_web(enable_json=True)
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
-    logger.addHandler(logging.StreamHandler(sys.stdout))
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(Formatter("%(asctime)s %(message)s"))
+    logger.addHandler(handler)
     return logger
 
 
