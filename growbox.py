9aba441f90ebef45ecb70313e6e83d52bede8281
Aleksandr Dmitriev <shurik2533@ya.ru>
2021-07-21 17:34:54 +0300

    fix

--- growbox.py
+++ growbox.py
@@ -19,6 +19,17 @@ from state import STATE, STATE_PATH
 
 
 def main():
+    def worker_main():
+        while True:
+            job_func = jobqueue.get()
+            try:
+                job_func()
+            except:
+                LOGGER.exception('')
+            jobqueue.task_done()
+
+    jobqueue = queue.Queue()
+
     try:
         def log_state():
             def default(o):
@@ -29,6 +40,10 @@ def main():
             save_state_to_db(state_string)
             joblib.dump(STATE, STATE_PATH)
 
+        def log_job_queue_size():
+            job_queue_len = len(jobqueue)
+            LOGGER.log(f'job_queue_len: {job_queue_len}')
+
         temperature_controller = TemperatureController(STATE)
         sensors_data_collector = SensorsDataCollector(STATE)
         light_controller = LightController(STATE)
@@ -36,15 +51,19 @@ def main():
         watering_controller_bottom = WateringController(STATE, 'bottom')
         heat_controller = HeatController(STATE)
 
-        schedule.every(10).seconds.do(temperature_controller.update_fan_pwm)
-        schedule.every(10).seconds.do(sensors_data_collector.get_data)
-        schedule.every(300).seconds.do(light_controller.control)
-        schedule.every().day.at("10:30").do(watering_controller_top.control)
-        schedule.every().day.at("10:30").do(watering_controller_bottom.control)
-        # schedule.every().day.at("20:30").do(watering_controller_top.control)
-        # schedule.every().day.at("20:30").do(watering_controller_bottom.control)
-        schedule.every(120).seconds.do(heat_controller.control)
-        schedule.every(10).seconds.do(log_state)
+        schedule.every(10).seconds.do(jobqueue.put, temperature_controller.update_fan_pwm)
+        schedule.every(10).seconds.do(jobqueue.put, sensors_data_collector.get_data)
+        schedule.every(300).seconds.do(jobqueue.put, light_controller.control)
+        schedule.every().day.at("10:30").do(jobqueue.put, watering_controller_top.control)
+        schedule.every().day.at("10:30").do(jobqueue.put, watering_controller_bottom.control)
+        # schedule.every().day.at("20:30").do(jobqueue.put, watering_controller_top.control)
+        # schedule.every().day.at("20:30").do(jobqueue.put, watering_controller_bottom.control)
+        schedule.every(120).seconds.do(jobqueue.put, heat_controller.control)
+        schedule.every(10).seconds.do(jobqueue.put, log_state)
+        schedule.every(10).seconds.do(jobqueue.put, log_job_queue_size)
+
+        worker_thread = threading.Thread(target=worker_main)
+        worker_thread.start()
 
         while True:
             schedule.run_pending()
